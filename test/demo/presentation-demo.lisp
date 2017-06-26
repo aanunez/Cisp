21cf727c8b0d740ace5f3c38b7f1d47fd64c922c
adam nunez <adam.a.nunez@gmail.com>
2017-06-26 14:02:41 -0500

    Prelim changes, issues documented in readme. Running in cobc 1.1, but with issues.

--- test/demo/presentation-demo.lisp
+++ test/demo/presentation-demo.lisp
@@ -1,7 +1,7 @@
 ;;; Cisp demo for presentation!
 ;;; Comemnts Work! Yay!
 (print "Hello!")
-(print "Numers:")  (print 42) 
-(print "Addition:") 
-(print (+ 1 2 (+ 3 5 10 (+ 20 1))))
-(print "Yay!")
+;;;(print "Numers:")  (print 42)
+;;;(print "Addition:")
+;;;(print (+ 1 2 (+ 3 5 10 (+ 20 1))))
+;;;(print "Yay!")
